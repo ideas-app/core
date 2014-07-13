296b0cdbbac0f89376f0cd308cfa5f7e8efab1ec
Valen Varangu Booth <valen@valenvb.ca>
2014-07-12 23:28:08 -0400

    vagrant bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,9 @@
 apt-get update
+apt-get -y install php5-gd
+apt-get -y install openssl
 rm -rf /var/www/html
 ln -fs /vagrant /var/www/html
 sudo service apache2 restart
+echo “**********************************************”
+echo “* Server is running at http://localhost:8080 *”
+echo “**********************************************”
